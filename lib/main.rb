a91e2280736e9818a3178f922a9bde82c5f135d4
jake park <jake.park@caa.columbia.edu>
2017-03-21 16:32:32 -0400

    require csv library

--- lib/main.rb
+++ lib/main.rb
@@ -1,6 +1,19 @@
+require 'csv'
+
 class Main
   def get_file(filepath)
+    arr = []
+    
+    lines = CSV.foreach(filepath) do |line|
+      arr.push(line)
+    end
+    
+    arr
   end
   
   
 end
+
+m = Main.new
+
+puts m.get_file("./input/1.csv")
