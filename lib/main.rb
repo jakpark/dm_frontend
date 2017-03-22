cb899708aeb5efc1e7a6069288c1737ab0aa9374
jake park <jake.park@caa.columbia.edu>
2017-03-22 16:54:28 -0400

    Format gender to full phrase

--- lib/main.rb
+++ lib/main.rb
@@ -31,10 +31,10 @@ class Main
     lines = CSV.new(body, col_sep: delimiter, headers: true, :header_converters => :symbol, :converters => [:all, :blank_to_nil])
     arr = lines.to_a.map do |row|
       row_h = row.to_hash
-      if row_h[:gender] == "Male"
-        row_h[:gender] = "M"
-      elsif row_h[:gender] == "Female"
-        row_h[:gender] = "F"
+      if row_h[:gender] == "M"
+        row_h[:gender] = "Male"
+      elsif row_h[:gender] == "F"
+        row_h[:gender] = "Female"
       end
       
       row_h
@@ -51,8 +51,25 @@ arr.concat(m.parse("./input/1.txt"))
 arr.concat(m.parse("./input/2.txt"))
 arr.concat(m.parse("./input/3.txt"))
 
-puts "sort1: "
-puts arr.sort_by { |k| [k[:gender], k[:lastname]]}
+puts "Output 1: "
+arr = arr.sort_by { |k| [k[:gender], k[:lastname]]}
+x = arr.each do |obj|
+  printrow = ""
+  printrow = obj[:lastname] + " " + obj[:firstname] + " " +
+    obj[:gender] + " " + obj[:dateofbirth] + " " + obj[:favoritecolor]
+  puts printrow
+end
+
+puts 
+"Hingis Martina Female 4/2/1979 Green
+Kelly Sue Female 7/12/1959 Pink
+Kournikova Anna Female 6/3/1975 Red
+Seles Monica Female 12/2/1973 Black
+Abercrombie Neil Male 2/13/1943 Tan
+Bishop Timothy Male 4/23/1967 Yellow
+Bonk Radek Male 6/3/1975 Green
+Bouillon Francis Male 6/3/1975 Blue
+Smith Steve Male 3/3/1985 Red"
 
 # puts "sort2: "
 # puts arr.sort_by { |k| [k[:DateOfBirth], k[:lastname]]}
