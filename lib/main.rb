e056242916408cccdb97145ecb7e69695b4909d7
jake park <jake.park@caa.columbia.edu>
2017-03-22 11:20:00 -0400

    refactor #parse to only take filepath argument. automatic delimiter parsing

--- lib/main.rb
+++ lib/main.rb
@@ -1,30 +1,31 @@
 require 'csv'
 
 class Main
-  def parse(filepath, separator = "comma")
-    case 
-    when separator == "space"
-      delimiter = " "
-    when separator == "comma"
-      delimiter = ", "
-    when separator == "pipe"
+  def parse(filepath)
+    header = File.open(filepath, &:readline)
+
+    if header =~ /\|/
       delimiter = " | "
+    elsif header =~ /\,/
+      delimiter = ", "
+    else
+      delimiter = " "
     end
-    
+
     arr = []
-      
+
     lines = CSV.foreach(filepath, col_sep: delimiter) do |line|
       arr.push(line)
     end
-    
+
     arr
   end
-  
-  
+
+
 end
 
 # m = Main.new
-# 
+#
 # puts m.parse("./input/1.csv", "space")
 # puts m.parse("./input/2.csv", "comma")
 # puts m.parse("./input/2.csv")
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -2,31 +2,25 @@ require './lib/main.rb'
 
 RSpec.describe Main, "Unit Tests:" do
   main = Main.new
-  
+
   context "Parse:" do
-    it "takes in only filepath and defaults to CSV" do
-      expect(main.parse("./lib/input/2.csv")).to eq (
-          [["Abercrombie", "Neil", "Male", "Tan", "2/13/1943"], ["Bishop", "Timothy", "Male", "Yellow", "4/23/1967"], ["Kelly", "Sue", "Female", "Pink", "7/12/1959"]]
-        )
+    it "parses files with delimiter: space" do
+      expect(main.parse("./lib/input/1.txt")).to eq (
+        [["Kournikova", "Anna", "F", "F", "6-3-1975", "Red"], ["Hingis", "Martina", "M", "F", "4-2-1979", "Green"], ["Seles", "Monica", "H", "F", "12-2-1973", "Black"]]
+      )
     end
-    
-    it "takes in filepath and delim: comma" do
-      expect(main.parse("./lib/input/2.csv", "comma")).to eq (
-          [["Abercrombie", "Neil", "Male", "Tan", "2/13/1943"], ["Bishop", "Timothy", "Male", "Yellow", "4/23/1967"], ["Kelly", "Sue", "Female", "Pink", "7/12/1959"]]
-        )
+
+    it "parses files with delimiter: comma" do
+      expect(main.parse("./lib/input/2.txt")).to eq (
+        [["Abercrombie", "Neil", "Male", "Tan", "2/13/1943"], ["Bishop", "Timothy", "Male", "Yellow", "4/23/1967"], ["Kelly", "Sue", "Female", "Pink", "7/12/1959"]]
+      )
     end
-    
-    it "takes in filepath and delim: space" do
-      expect(main.parse("./lib/input/1.csv", "space")).to eq (
-          [["Kournikova", "Anna", "F", "F", "6-3-1975", "Red"], ["Hingis", "Martina", "M", "F", "4-2-1979", "Green"], ["Seles", "Monica", "H", "F", "12-2-1973", "Black"]]
-        )
+
+    it "parses files with delimiter: pipe" do
+      expect(main.parse("./lib/input/3.txt")).to eq (
+        [["Smith", "Steve", "D", "M", "Red", "3-3-1985"], ["Bonk", "Radek", "S", "M", "Green", "6-3-1975"], ["Bouillon", "Francis", "G", "M", "Blue", "6-3-1975"]]
+      )
     end
-    
-    it "takes in filepath and delim: pipe" do
-      expect(main.parse("./lib/input/3.csv", "pipe")).to eq (
-          [["Smith", "Steve", "D", "M", "Red", "3-3-1985"], ["Bonk", "Radek", "S", "M", "Green", "6-3-1975"], ["Bouillon", "Francis", "G", "M", "Blue", "6-3-1975"]]
-        )
-    end
-    
+
   end
 end
